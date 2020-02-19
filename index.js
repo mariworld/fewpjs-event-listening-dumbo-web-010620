9ceeba2cdd3d37763cfacd12a599f24243165c87
Mari <demarius.edwards@gmail.com>
2020-02-19 14:18:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
+function addingEventListener(event,callback) {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(event) {
+    alert('I was clicked!')});
 }
+addingEventListener();
